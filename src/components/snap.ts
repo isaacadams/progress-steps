7d8a5f0676838e7acd6b79d61ddb83f5e195a191
Isaac Adams <isaac.d.adams@gmail.com>
2021-06-04 22:39:18 -0400

    making the svg more scalable

--- src/components/snap.ts
+++ src/components/snap.ts
@@ -1,13 +1,19 @@
 import Snap from 'snapsvg';
 import settings from '../settings';
 
+const [w, h] = getRectangle(20);
+
+function getRectangle(s: number) {
+  return [s, s / 6];
+}
+
 const s = Snap(settings.id);
 s.attr({
   viewBox: [-5, 0, 250, 40].join(' '),
   x: 0,
   y: 0,
-  height: 'unset',
-  width: 'unset',
+  height: h + 'rem',
+  width: w + 'rem',
   preserveAspectRatio: 'none',
 });
 
