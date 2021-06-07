ad9a1792e9bc38be4e3d54d3b08066daa92f75e0
Isaac Adams <isaac.d.adams@gmail.com>
2021-06-07 17:43:47 -0400

    wrapped it in a method

--- src/progress.ts
+++ src/progress.ts
@@ -10,23 +10,30 @@ addImagePatternToDefs('check-mark', checkMark);
 export const steps = [createStep(1), createStep(2), createStep(3)];
 let [one, two, three] = steps;
 
-// only need connecting lines between one and two
-// the path/line should go first
-// everything that appears after will be drawn on top of line
-one.drawLine();
-two.drawLine();
+function drawProgressBar() {
+  // only need connecting lines between one and two
+  // the path/line should go first
+  // everything that appears after will be drawn on top of line
+  one.drawLine();
+  two.drawLine();
 
-let cursorX = settings.beginAt;
+  let cursorX = settings.beginAt;
 
-addStepToProgressBar(cursorX, one.stepSVG);
-addStepToProgressBar(cursorX, two.stepSVG);
-addStepToProgressBar(cursorX, three.stepSVG);
+  addStepToProgressBar(cursorX, one.stepSVG);
+  addStepToProgressBar(cursorX, two.stepSVG);
+  addStepToProgressBar(cursorX, three.stepSVG);
 
-function addStepToProgressBar(x: number, group: Element) {
-  group.attr({ class: 'progress-bar-step', transform: 'translate(' + [x, 0].join(',') + ')' });
-  paper.append(group);
-  cursorX = cursorX + settings.distanceBetweenCircles;
+  function addStepToProgressBar(x: number, group: Element) {
+    group.attr({
+      class: 'progress-bar-step',
+      transform: 'translate(' + [x, 0].join(',') + ')',
+    });
+    paper.append(group);
+    cursorX = cursorX + settings.distanceBetweenCircles;
+  }
 }
+
+drawProgressBar();
 
 export function completeStepOne() {
   one.complete();
